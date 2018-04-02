112c7f4895b232d5b70b614a06aa35895e8923e5
KennyCruzer <kennycruz@ool-ad020785.dyn.optonline.net>
2018-04-02 08:22:09 -0400

    update

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ let $form = $('form');
 let $board = $('#board');
 let yourName = "";
 let $matrix;
+let sq1 = []; 
+let sq2 = []; 
 let x = true; 
 let colors = ["rgb(218, 148, 255)", "rgb(174, 165, 255)", "rgb(118, 187, 255)", "rgb(57, 209, 255)"];
 
@@ -27,7 +29,7 @@ function colorMatrix(){
 		$matrix = $("<div></div>");
 		$matrix.addClass('matrix ' + row );
 		$matrix.attr('id', i);
-//		$matrix.click(swap);
+		$matrix.click(swap);
 		$matrix.css('background', colors[Math.floor(i/colors.length)]);
 		$board.append($matrix);
 	}
@@ -48,18 +50,30 @@ function colorScrambler(){
 }
 
 function swap(){
-	let sq1 = $(this);
-	if()
-	let sq1 = $(this);
-	let sq2 = $(this);
-	console.log(sq1);
-	console.log(sq2);
+	if (x === true){
+		sq1.pop();
+		sq1.push($(this)); 
+		sq1.forEach(() => sq1[0].addClass('selected'));
+//		debugger;
+		return x = false;
+	}else if (($(this) === sq1[0])&&(x === false)){
+		sq1.forEach(() => sq1[0].removeClass('selected'));
+		sq1.pop();
+//		debugger; 
+		return x = true;
+		}
+	else{
+		sq2.pop();
+		debugger; 
+		sq2.push($(this)); 
+		sq2.forEach(() => sq2[0].addClass('selected'));
+		return x = true; 
+		}
 }
 
 
 
 colorMatrix(); 
 colorScrambler(); 
-//swap();
 //});
 
